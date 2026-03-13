f62d3fd3bdb752cbbee09f5381aad79a6cfe77bc
Srinivaspandrala <rschintu3g@gmail.com>
2026-03-13 12:37:08 +0530

    update db

--- db.js
+++ db.js
@@ -1,19 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const parseBool = (value, defaultValue = false) => {
-  if (value === undefined || value === null || value === "") return defaultValue;
-  return ["1", "true", "yes", "on"].includes(String(value).toLowerCase());
-};
-
-const sslEnabled = parseBool(process.env.DB_SSL, true);
-const sslRejectUnauthorized = parseBool(
-  process.env.DB_SSL_REJECT_UNAUTHORIZED,
-  true
-);
-const sslCa =
-  process.env.DB_SSL_CA && process.env.DB_SSL_CA.replace(/\\n/g, "\n");
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -25,13 +12,7 @@ const pool = new Pool({
   idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MS, 10) || 30000,
   connectionTimeoutMillis:
     parseInt(process.env.DB_CONN_TIMEOUT_MS, 10) || 10000,
-  // enable SSL by default (set DB_SSL=false to disable)
-  ssl: sslEnabled
-    ? {
-        rejectUnauthorized: sslRejectUnauthorized,
-        ...(sslCa ? { ca: sslCa } : {}),
-      }
-    : false,
+  ssl: false,
 });
 
 pool.on("error", (err) => {
